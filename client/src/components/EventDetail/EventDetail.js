0d791008fedccd5ae12ca7bf532d30e2c43799e5
oliver <oliverzh06009@gmail.com>
2022-09-19 22:36:17 +1000

    create files

---
+++ client/src/components/EventDetail/EventDetail.js
@@ -0,0 +1,22 @@
+import React, {useEffect, useState} from "react";
+import {Container} from "@mui/material";
+
+
+export default function EventDetail() {
+
+        return(
+
+            <div>
+                <Container>
+                            1111
+                </Container>
+
+
+            </div>
+
+
+
+        );
+
+
+}
---
+++ client/src/components/EventDetail/ParticipantCPN.js
@@ -0,0 +1,19 @@
+import React, {useEffect, useState} from "react";
+
+export default function ParticipantCPN() {
+
+    return(
+
+        <div>
+
+            111111
+
+
+        </div>
+
+
+
+    );
+
+
+}
