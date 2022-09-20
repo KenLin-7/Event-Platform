174b73b28e072cc6b467543db2bdf1815f074a06
Qucheng Zhang <qzha0429@uni.sydney.edu.au>
2022-09-20 20:51:56 +1000

    added top bar

---
+++ client/src/components/Header.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import AdbIcon from '@mui/icons-material/Adb';
+import { Stack } from '@mui/system';
+import { Button, Typography } from '@mui/material';
+
+import '../asserts/stylesheet/header.css'
+
+const items = [
+  { id: 1, text: 'Latest event' },
+  { id: 2, text: 'Search' },
+  { id: 3, text: 'Community' },
+  { id: 4, text: 'About us' },
+];
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <Container maxWidth="xl" sx={{ display: "flex", marginTop: 3 }}>
+
+        <Box className="left">
+          <Stack direction="row" spacing={2}>
+            <AdbIcon />
+            <Typography className="header-logo-font">
+              Event Plaza
+            </Typography>
+          </Stack>
+        </Box>
+
+        <div className='mid'>
+          {
+            items.map(item => {
+              return (
+                <Button key={item.id} className="header-mid-btn">
+                  <Typography className="header-font" sx={{ color: 'black', fontSize: 'small' }}>
+                    {item.text}
+                  </Typography>
+                </Button>
+              )
+            })
+          }
+        </div>
+
+        <div className="right">
+          <Stack className="right-stack" direction="row" spacing={2}>
+            <Button variant="outlined" sx={{ borderRadius: 8 }}>Sign in</Button>
+            <Button variant="contained" sx={{ borderRadius: 8 }}>Register</Button>
+          </Stack>
+        </div>
+
+
+      </Container>
+    </div>
+  )
+}
+
+export default Header
---
+++ client/src/components/HomePage/Search.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <section className='py-4 container'>
+      <div className="row justify-content-center">
+
+        <div className='col-11 col-md-6 co-lg-3 mx-0 mb-4'>
+          <div className='card p-0 overflow-hidden h-100 shadow'>
+
+          </div>
+
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Search
