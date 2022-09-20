b44e6a15a89ed02c85bf53f6c0e4c87e3692b95e
oliver <oliverzh06009@gmail.com>
2022-09-20 22:10:23 +1000

    update structure

--- client/src/components/EventDetail/EventDetail.js
+++ client/src/components/EventDetail/EventDetail.js
@@ -1,14 +1,43 @@
 import React, {useEffect, useState} from "react";
-import {Container} from "@mui/material";
+import {Card, CardMedia, Container, Paper, Stack, Typography} from "@mui/material";
+import img from "./xxxxxxxx.png"
+
+
+
+
 
 
 export default function EventDetail() {
 
+
+
         return(
 
             <div>
-                <Container>
-                            1111
+                <Container  maxWidth={"lg"}>
+                       <Stack spacing={3} sx={{margin:8, padding: 1}}>
+                            <Paper  justifyContent="space-evenly" sx={{margin:6, padding: 2}} >
+
+                                     <Stack direction="row" spacing={3}  >
+
+
+                                            <Card>
+                                                <CardMedia
+                                                    component="img"
+                                                    height="500"
+                                                    image={img}
+                                                    alt="eventpic"
+                                                />
+                                            </Card>
+                                          <Typography fontSize={25}> EVENT NAME </Typography>
+
+                                        </Stack>
+
+                             </Paper>
+
+                           <Stack> detail  </Stack>
+                           <Stack > participants </Stack>
+                       </Stack>
                 </Container>
 
 
