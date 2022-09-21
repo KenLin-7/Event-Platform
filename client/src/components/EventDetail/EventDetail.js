a859a3391950812ed5a1e0f09f0f7013485dfaa9
oliver <oliverzh06009@gmail.com>
2022-09-21 20:55:54 +1000

    draft of eventdetail page

--- client/src/components/EventDetail/EventDetail.js
+++ client/src/components/EventDetail/EventDetail.js
@@ -14,24 +14,24 @@ import {
 } from "@mui/material";
 import img from "./xxxxxxxx.png"
 import Grid2 from "@mui/material/Unstable_Grid2";
+import ParticipantCPN from "./ParticipantCPN";
 
 
 export default function EventDetail() {
 
-    const eventIntro= "See the night sky light up with spectacular fireworks displays every Saturday night.\n" +
+    const eventIntro = "See the night sky light up with spectacular fireworks displays every Saturday night.\n" +
         "\n" +
         "Grab yourself a ringside seat around Cockle Bay, or a bite to eat in one of the many harbour side restaurants, and enjoy an amazing fireworks display every Saturday night in Darling Harbour!\n" +
         "\n" +
         "Time: 8.30pm then at 9pm from 8 October 2022"
 
 
-
     return (
 
         <div>
             <Container maxWidth={"lg"}>
-                <Stack  sx={{padding: 1 ,marginX:20, marginBottom:10}}>
-                    <Paper justifyContent="space-evenly" sx={{padding:2}} >
+                <Stack sx={{padding: 1, marginX: 20, marginBottom: 10}}>
+                    <Paper justifyContent="space-evenly" sx={{padding: 2}}>
                         <Box sx={{flexGrow: 1}}>
                             <Grid2 container spacing={2}>
                                 <Grid2 xs={6}>
@@ -49,16 +49,19 @@ export default function EventDetail() {
                                     <Stack direction={"row"} sx={{marginLeft: 2, marginTop: 8}} spacing={1}>
                                         <Avatar sx={{width: 50, height: 50}} src={img}/>
                                         <Stack>
-                                            <Typography align={"left"} fontSize={16} > Hoster name </Typography>
-                                            <Typography fontSize={12} fontWeight={500}> @xxxxx Account Number </Typography>
+                                            <Typography align={"left"} fontSize={16}> Hoster name </Typography>
+                                            <Typography align={"left"} fontSize={12} fontWeight={500}> @xxxxx Account
+                                                Number </Typography>
                                         </Stack>
                                     </Stack>
 
                                     <Stack sx={{marginTop: 5, marginX: 2}}>
-                                        <Card elevation={5}  >
-                                            <Typography  fontSize={25}  sx={{ marginTop:4,marginBottom:2}}> EVENT NAME </Typography>
+                                        <Card elevation={5}>
+                                            <Typography fontSize={25} sx={{marginTop: 4, marginBottom: 2}}> EVENT
+                                                NAME </Typography>
                                             <Typography fontWeight={500}> Event Time </Typography>
-                                            <Button variant="contained" size="large" sx={{ marginY:5}} >Regist Now</Button>
+                                            <Button variant="contained" size="large" sx={{marginY: 5}}>Regist
+                                                Now</Button>
                                         </Card>
                                     </Stack>
 
@@ -69,42 +72,25 @@ export default function EventDetail() {
                     <Stack sx={{marginTop: 5}}>
 
                         <Typography fontSize={25} fontWeight={500}> Event Introduction </Typography>
-                        <Typography fontSize={18} sx={{ marginTop:2}} > {eventIntro} </Typography>
+                        <Typography fontSize={18} sx={{marginTop: 2}}> {eventIntro} </Typography>
 
                     </Stack>
 
-                    <Divider sx={{marginTop: 3}} variant="middle" />
+                    <Divider sx={{marginTop: 3}} variant="middle"/>
 
 
-                    <Stack sx={{marginTop:3, marginLeft:6}} >
+                    <Stack sx={{marginTop: 3, marginX: 6}}>
                         <Typography align={"left"} fontSize={18} fontWeight={500}>Participants: </Typography>
-                        <Stack direction={"row"} sx={{marginTop:2, marginLeft:2}} spacing={2}>
+                        <Grid2 container direction={"row"} sx={{marginTop: 2, marginX: 2}} spacing={2}>
+                            <ParticipantCPN></ParticipantCPN>
+                            <ParticipantCPN></ParticipantCPN>
+                            <ParticipantCPN></ParticipantCPN>
+                            <ParticipantCPN></ParticipantCPN>
+                            <ParticipantCPN></ParticipantCPN>
+                            <ParticipantCPN></ParticipantCPN>
+                            <ParticipantCPN></ParticipantCPN>
 
-                            <Chip
-                                avatar={<Avatar sx={{width: 50, height: 50}} src={img} alt="User Name"/>}
-                                label="User name"
-                                variant="outlined"
-                                size="medium"
-                            />
-                            <Chip
-                                avatar={<Avatar sx={{width: 50, height: 50}} src={img} alt="User Name"/>}
-                                label="User name"
-                                variant="outlined"
-                                size="medium"
-                            />
-                            <Chip
-                                avatar={<Avatar sx={{width: 50, height: 50}} src={img} alt="User Name"/>}
-                                label="User name"
-                                variant="outlined"
-                                size="medium"
-                            />
-                            <Chip
-                                avatar={<Avatar sx={{width: 50, height: 50}} src={img} alt="User Name"/>}
-                                label="User name"
-                                variant="outlined"
-                                size="medium"
-                            />
-                        </Stack>
+                        </Grid2>
                     </Stack>
                 </Stack>
             </Container>
--- client/src/components/EventDetail/ParticipantCPN.js
+++ client/src/components/EventDetail/ParticipantCPN.js
@@ -1,17 +1,19 @@
 import React, {useEffect, useState} from "react";
+import Grid2 from "@mui/material/Unstable_Grid2";
+import {Avatar, Chip} from "@mui/material";
+import img from "./xxxxxxxx.png"
 
 export default function ParticipantCPN() {
 
     return(
-
-        <div>
-
-            111111
-
-
-        </div>
-
-
+            <Grid2>
+                <Chip
+                    avatar={<Avatar sx={{width: 50, height: 50}} src={img} alt="User Name"/>}
+                    label="User name"
+                    variant="outlined"
+                    size="medium"
+                />
+            </Grid2>
 
     );
 
