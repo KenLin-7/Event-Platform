fa499c065136e06b2d858c838461772115a234a2
kenlin <glin4517@uni.sydney.edu.au>
2022-09-23 00:32:54 +1000

    update header button

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -53,11 +53,11 @@ const Header = () => {
         <div className={styles["right"]}>
           {auth === null?(            
           <Stack className={styles["right-stack"]} direction="row" spacing={2}>
-            <Link to="/login"><Button variant="outlined" sx={{ borderRadius: 8 }} className="header-btn">Sign in</Button></Link>
-            <Link to="/register"><Button variant="contained" sx={{ borderRadius: 8 }} className="header-btn">Register</Button></Link>
+            <Link to="/login"><Button variant="outlined" sx={{ borderRadius: 2 }} className="header-btn">Sign in</Button></Link>
+            <Link to="/register"><Button variant="contained" sx={{ borderRadius: 2 }} className="header-btn">Register</Button></Link>
           </Stack>):(
           <Stack className={styles["right-stack"]} direction="row" spacing={2}>
-            <Button variant="outlined" sx={{ borderRadius: 8 }} onClick={signOut} className="header-btn">Log out</Button>
+            <Button variant="outlined" sx={{ borderRadius: 2 }} onClick={signOut} className="header-btn">Log out</Button>
           </Stack>
           )
           }
