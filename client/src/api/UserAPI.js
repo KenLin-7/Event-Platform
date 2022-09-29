50bf271920528fc269f25174022ff749aee6d53d
s3633197 <jzha8715@uni.sydney.edu.au>
2022-09-29 16:49:39 +0800

    bug fix

--- client/src/api/UserAPI.js
+++ client/src/api/UserAPI.js
@@ -3,8 +3,8 @@ const API = '/api/user'
 
 export const signIn = async (email,password)=>{
     const formData = new FormData()
-    formData.append("username","wasd@wasd.com")
-    formData.append("password","123456")
+    formData.append("username",email)
+    formData.append("password",password)
     const result = await axios.post("/login",formData,{headers:{"content-type":"application/x-www-form-urlencoded"}})
     return result
 }
@@ -13,18 +13,6 @@ export const signUp = async (user)=>{
     const result = await axios.post(API+"/register",user)
     return result
 }
-
-
-export const getInfo  = async (email)=>{
-    const result = await axios.get("/test/info",{params:{email}})
-    return result 
-}
-
-export const getUser = async (email)=>{
-    const result = await axios.post(API+"/current",{params:{email}})
-    return result 
-}
-
 export const profile = async (email)=>{
     const result = await axios.post(API+"/profile",{params:{email}})
     return result 
@@ -32,6 +20,16 @@ export const profile = async (email)=>{
 
 export const updateUser = async (user)=>{
     const result = await axios.post(API+"/updateUser",user)
+    return result 
+}
+
+export const getInfo  = async (email)=>{
+    const result = await axios.get("/test/info",{params:{email}})
+    return result 
+}
+
+export const getUser = async ()=>{
+    const result = await axios.post(API+"/current")
     return result 
 }
 
